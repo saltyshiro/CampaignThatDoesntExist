64f7b13820352b9685c5ba4861113e98a8230a4f
saltyshiro <108318806+saltyshiro@users.noreply.github.com>
2026-03-08 15:57:41 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "saltyshiro.github.io/CampaignThatDoesntExist",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
